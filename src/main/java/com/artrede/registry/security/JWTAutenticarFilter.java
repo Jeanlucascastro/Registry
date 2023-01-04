e74f559853da510256ab9a942dd5f14b663c58eb
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2023-01-04 17:00:08 -0300

    continue

--- src/main/java/com/artrede/registry/security/JWTAutenticarFilter.java
+++ src/main/java/com/artrede/registry/security/JWTAutenticarFilter.java
@@ -1,14 +1,28 @@
 package com.artrede.registry.security;
 
+import com.artrede.registry.data.DetalheUsuarioData;
 import com.artrede.registry.entities.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Date;
+
 public class JWTAutenticarFilter extends UsernamePasswordAuthenticationFilter {
+
+    public static final int TOKEN_EXPIRACAO = 600_000;
+    public static final String TOKEN_SENHA = "7bd33504-3670-4128-85e1-a938b98b9e88";
 
     private final AuthenticationManager authenticationManager;
 
@@ -17,9 +31,37 @@ public class JWTAutenticarFilter extends UsernamePasswordAuthenticationFilter {
         this.authenticationManager = authenticationManager;
     }
 
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
-        Usuario usuario 
+        try {
+            Usuario usuario = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    usuario.getUserName(),
+                    usuario.getPassword(),
+                    new ArrayDeque<>()
+            ));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Falha ao autenticar usuario ", e);
+        }
+
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        DetalheUsuarioData usuarioData = (DetalheUsuarioData) authResult.getPrincipal();
+
+        String token = JWT.create()
+                .withSubject(usuarioData.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TOKEN_EXPIRACAO))
+                .sign(Algorithm.HMAC512(TOKEN_SENHA));
+
+        response.getWriter().write(token);
+        response.getWriter().flush();
+
     }
 }
---
+++ src/main/java/com/artrede/registry/security/JWTConfiguracao.java
@@ -0,0 +1,48 @@
+package com.artrede.registry.security;
+
+import com.artrede.registry.services.DetalherUsuarioServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
+
+    private final DetalherUsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    public JWTConfiguracao(DetalherUsuarioServiceImpl usuarioService, PasswordEncoder passwordEncoder) {
+        this.usuarioService = usuarioService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAutenticarFilter(authenticationManager()))
+                .addFilter(new JWTAutenticarFilter(authenticationManager()))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfiguration corsConfiguration() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+    return source;
+    }
+}
